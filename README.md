038d2242ed9b37aca1743939d1e3ae66019a1d87
Corbin Crutchley <git@crutchcorn.dev>
2026-01-30 06:19:01 -0800

    chore: self cert

---
+++ README.md
@@ -0,0 +1,13 @@
+# Setup
+
+```shell
+brew install mkcert
+brew install nss # if you use Firefox
+mkcert -install
+```
+
+# Usage
+
+```shell
+mkcert localhost
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello")
+import { promises as fs } from 'fs';
+import path from 'path';
+import { createServer } from 'https';
+import { WebSocketServer } from 'ws';
+
+const cert = await fs.readFile(path.join(import.meta.dirname, "../localhost.pem"), 'utf-8');
+const key = await fs.readFile(path.join(import.meta.dirname, "../localhost-key.pem"), 'utf-8');
+
+const server = createServer({
+  cert: cert,
+  key: key
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+server.listen(8080);
+
+server.on('error', console.error);
